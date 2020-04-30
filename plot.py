c36116081a0dbeb10dbbca754f542df7d95bf3f2
osheng <osheng@gmail.com>
2020-04-30 19:47:43 -0400

    added line of best fit

--- plot.py
+++ plot.py
@@ -1,14 +1,17 @@
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
 from constants import *
 from matplotlib import pyplot as plt
 from sys import argv
 from LSA import LSA
+from sklearn import linear_model
 
 # assert len(argv) == 3 "Usage: python3 plot.py CSV_NAME.csv CSV_NAME.csv"
 # for i in range(1,3):
 #     assert len(argv[1]) > 4 and argv[1][-4:] == ".csv", "Usage: python3 plot.py CSV_NAME.csv CSV_NAME.csv"
-argv = ["plot.py","SPTSXComp.csv","XIC.csv"]
+
+# For testing...
+argv = ["plot.py", "SPTSXComp.csv", "XIC.csv"]
 
 x_name = argv[1][:-4]
 y_name = argv[2][:-4]
@@ -16,14 +19,16 @@ y_name = argv[2][:-4]
 x = pd.read_csv(RAW + argv[1])
 y = pd.read_csv(RAW + argv[2])
 
-data = x.merge(y, on=DATE)
-data = data[[DATE, ACLO+"_x", ACLO+"_y"]].dropna(axis=0)
-plot = sb.scatterplot(x=data[ACLO + "_x"], y=data[ACLO + "_y"], data=data, color="orange")
-plt.xlabel(x_name)
-plt.ylabel(y_name)
-x = data[ACLO+"_x"]
-y = data[ACLO + "_y"]
-v = LSA(x, y)
+data = x.merge(y, on=DATE).rename(columns={ACLO+"_x": x_name, ACLO+"_y": y_name})
+data = data[[DATE, x_name, y_name]].dropna(axis=0)
 
-plt.plot([min(x), max(x)], [v[0]*min(x)+v[1],v[0]*max(x)+v[1]] , linewidth=2,color="blue")
-plt.show(plot)
+
+g = sns.FacetGrid(data)
+g = g.map(sns.scatterplot, x_name, y_name, edgecolor="white")
+x = data[x_name]
+y = data[y_name]
+v = LSA(x, y, n=1)
+X = np.arange(min(x), max(x))
+Y = v[0]*X+v[1]
+plt.plot(X,Y,color="red")
+plt.show(g)
