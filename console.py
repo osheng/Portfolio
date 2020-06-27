4aca8dd699b7f40ed3b24332c9de035bcb4fe241
osheng <osheng@gmail.com>
2020-06-27 16:29:03 -0400

    wrote run

--- console.py
+++ console.py
@@ -1,9 +1,12 @@
 # To explore the data in console run the following
 # exec(open("console.py").read())
 import pandas as pd
+import datetime
+from pandas import Series
 import seaborn as sns
 from constants import *
 import numpy as np
+from asset import Asset
 from numpy.linalg import inv
 WELCOME = "The console is now set up and ready for you to explore data!"
 TSX_data = pd.read_csv(RAW + TSX)
@@ -12,6 +15,13 @@ print(WELCOME)
 
 data = TSX_data.merge(XIC_data, on=DATE)
 
-data = data[[DATE,ACLO+"_x",ACLO+"_y"]].dropna(axis=0)
+data = data[[DATE, ACLO+"_x", ACLO+"_y"]].dropna(axis=0)
 x = data[ACLO + "_x"]
 y = data[ACLO + "_y"]
+
+
+def run(file_name: str) -> None:
+    exec(open(file_name).read())
+
+
+a = Asset(name=TSX, history_fn=RAW+TSX)
